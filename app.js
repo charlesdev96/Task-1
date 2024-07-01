0781618c1d80f9e21f62f64f91aa3599667b34e7
Charles Ogonnaya <ogonnayacn@gmail.com>
2024-07-01 23:30:41 +0100

    port

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get("/api/hello", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
